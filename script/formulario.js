1ceb126f65cc3c5800a7fd03f45189f57a50ed29
Nicolas <nicolastimoteu@gmail.com>
2024-04-25 22:37:52 -0400

    Adicionado a opção de alterar produto

--- script/formulario.js
+++ script/formulario.js
@@ -1,20 +1,36 @@
-const formulario = document.querySelector("#formulario_adicionar")
+const adicionarProduto = document.querySelector("#formulario_adicionar")
 const removerProduto = document.querySelector('#formulario_remover')
+const alterarProduto = document.querySelector("#formulario_alterar")
 
 const adicionar = document.querySelector(".adicionar")
 const remover = document.querySelector(".remover")
+const alterar = document.querySelector(".alterar")
+
+const inputAlterar = document.querySelector('#id-produto-procurar')
+
+function esconderFormulario() {
+    [adicionarProduto, removerProduto, alterarProduto].forEach((formulario) => {
+        formulario.style.display = 'none'
+    })
+}
 
 adicionar.addEventListener('click', () => {
-    removerProduto.style.display = 'none'
-    formulario.style.display = 'flex'
+    esconderFormulario()
+    adicionarProduto.style.display = 'flex'
 })
 
 remover.addEventListener('click', () => {
+    esconderFormulario()
     removerProduto.style.display = 'flex'
-    formulario.style.display = 'none'
- })
 
-formulario.addEventListener('submit', async function (event) {
+})
+
+alterar.addEventListener('click', () => {
+    esconderFormulario()
+    alterarProduto.style.display = 'flex'
+})
+
+adicionarProduto.addEventListener('submit', async function (event) {
     event.preventDefault()
 
     const nome = document.querySelector("#nome")
@@ -60,4 +76,66 @@ removerProduto.addEventListener('submit', async function (event) {
 
 
     await alert('arquivo deletado!');
+})
+
+async function buscarProduto(id) {
+
+    try {
+        const produto = await fetch(`http://localhost:3000/produtos/${id}`)
+        const produtoJson = await produto.json()
+
+        const nome = document.getElementById("nome-alterar")
+        console.log(nome.value)
+        if (nome === "") {
+            throw Error("Produto não encontrado")
+        } else {
+            nome.value = produtoJson.nome
+            nome.removeAttribute('disabled')
+
+            const imageAlterar = document.getElementById("image-alterar")
+            imageAlterar.value = produtoJson.image
+            imageAlterar.removeAttribute('disabled')
+
+            const precoAlterar = document.getElementById("preco-alterar")
+            precoAlterar.value = produtoJson.preço
+            precoAlterar.removeAttribute('disabled')
+
+            const quantidadeAlterar = document.getElementById("quantidade-alterar")
+            quantidadeAlterar.value = produtoJson.quantidade
+            quantidadeAlterar.removeAttribute('disabled')
+        }
+
+    } catch (error) {
+        alert(`${error}`)
+    }
+
+}
+
+inputAlterar.addEventListener('focusout', () => {
+    buscarProduto(inputAlterar.value)
+})
+
+alterarProduto.addEventListener('submit', async (event) => {
+    event.preventDefault()
+
+    const nome = document.getElementById("nome-alterar").value
+    const imageAlterar = document.getElementById("image-alterar").value
+    const precoAlterar = document.getElementById("preco-alterar").value
+    const quantidadeAlterar = document.getElementById("quantidade-alterar").value
+
+    const produtoAlterado = {
+        'nome': nome,
+        'image': [imageAlterar],
+        "preço": precoAlterar,
+        "quantidade": quantidadeAlterar
+    }
+
+    await fetch(`http://localhost:3000/produtos/${inputAlterar.value}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(produtoAlterado)
+    })
+
 })
