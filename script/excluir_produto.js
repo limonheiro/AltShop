228cdb94288b79c927fc390564d91fc40a33a997
Nicolas <nicolastimoteu@gmail.com>
2024-05-07 18:59:10 -0400

    Corrigido bug do delete

--- script/excluir_produto.js
+++ script/excluir_produto.js
@@ -1,7 +1,7 @@
 export default async function excluirProduto(id) {
 
     // await fetch(`http://localhost:3000/produtos/${id}`, {
-    await fetch(`https://alt-shop-sage.vercel.app/api/produtos${id}`, {
+    await fetch(`https://alt-shop-sage.vercel.app/api/produtos/${id}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
